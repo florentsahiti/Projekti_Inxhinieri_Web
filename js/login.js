97b91db082434f66816abbdcb6c07d005e891c1d
Florent Sahiti <florentsahiti06@gmail.com>
2022-12-25 19:52:16 +0100

    Validimi finish

--- js/login.js
+++ js/login.js
@@ -16,3 +16,54 @@ for(let i=0;i<tabHeaderElements.length;i++){
         tabBodyElements[i].classList.add("active");
     });
 }
+
+
+function validoMeRegex() {
+    //shkronja e pare e madhe dhe me pas nje numer
+    var usernameRegex =  /^[A-Z]{1}[a-zA-Z0-9]+[0-9]{1}$/; //REGEX
+    var pwRegex = /^[A-Z][a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]{6,}$/ //
+    
+    var useri = document.getElementById('useri').value;
+    
+    var pw  = document.getElementById('pw').value;
+    
+    if(usernameRegex.test(useri)){
+        console.log("Username eshte ne rregull");
+    }else{
+        console.log("Username nuk eshte ne rregull");
+    }
+    if(pwRegex.test(pw)){
+        console.log("Passi eshte ne rregull")
+    }else{
+        console.log("Passi nuk eshte ne rregull")
+    }
+   
+}
+
+function validoMeRegex2() {
+    //shkronja e pare e madhe dhe me pas nje numer
+    var usernameRegex =  /^[A-Z]{1}[a-zA-Z0-9]+[0-9]{1}$/; //REGEX
+    var pwRegex = /^[A-Z][a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]{6,}$/ //
+    var emailRegex = /^[a-zA-Z][a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\.(com|net)$/;
+    
+    var useri = document.getElementById('useri').value;
+    
+    var pw  = document.getElementById('pw').value;
+    var email  = document.getElementById('emaili').value;
+
+    if(usernameRegex.test(useri)){
+        console.log("Username eshte ne rregull");
+    }else{
+        console.log("Username nuk eshte ne rregull");
+    }
+    if(pwRegex.test(pw)){
+        console.log("Passi eshte ne rregull")
+    }else{
+        console.log("Passi nuk eshte ne rregull")
+    }
+    if(emailRegex.test(email)){
+        console.log("emailio eshte ne rregull")
+    }else{
+        console.log("emaili nuk eshte ne rregull")
+    }
+}
